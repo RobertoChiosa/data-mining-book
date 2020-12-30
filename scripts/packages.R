07d891b7ce01b2e445f7b9e0a0eed83acb751fe0
Roberto Chiosa <robi@MacBook-Air-di-Roberto.local>
2020-12-30 20:09:43 +0100

    structure built, first content added

---
+++ scripts/packages.R
@@ -0,0 +1,37 @@
+
+#  LOAD PACKAGES ------------------------------------------------------------------
+
+library(tidyverse)              # data manipulation 
+library(ggplot2)              # data visualization
+# # library(dplyr)                # can be avoided loading tidyverse
+# # library(tidyr)                # can be avoided loading tidyverse
+# library(plyr)                   # data manipulation 
+library(ggExtra)                # distributions on border
+# library(RColorBrewer)           # Pacchetto per creare palette di colori più accattivanti
+# library(rpart)                  # make classification tree
+# library(rpart.plot)             # plot classification tree
+# library(partykit)               # regression and classification tree plot and tools
+library(scales)                 # Graphical scales map data to aesthetics
+# library(MLmetrics)              # measure regression, classification and ranking performance.
+library(lubridate)              # easy to work with dates and times
+# library(imputeTS)               # manage missing values
+# library(jmotif)                 # sax manipulations
+ library(ggpubr)                 # multiple plots in line or column
+# library(stringr)                # multiple plots in line or column
+# library(networkD3)              # draw sankey plots
+# library(NbClust)                # cluster validation measures
+# library(knitr)
+# library(magrittr)
+# library(dendextend)
+# library(ggsci)
+# library(zoo)
+# library(qcc)
+# library(Hmisc)                  # permette di riportare info in lag
+# library(evtree)                 # labelling without overlap
+# library(moments)                # para uso das kewness e kurtosis
+# library(partykit)               # to draw and display trees
+# library(party)                  # to draw and display trees
+# library(arules)                 # to make association rules
+# library(treemap)                # to make tree plot
+# library(arulesViz)              # to draw association rules graphs
+# library(rjson)                  # read json files
---
+++ scripts/processing.R
@@ -0,0 +1,61 @@
+source("./scripts/packages.R") # load necessary packages
+df <- read.csv("./data/data2.csv", header = TRUE, sep = ",", dec = ".", check.names = FALSE)
+
+df <- df %>%
+  mutate(
+    Date_Time = as.POSIXct(Date_Time , format = "%Y-%m-%d %H:%M:%S" , tz = "Europe/Rome"), # depend on selected timezone
+    Week_Day = wday(Date_Time, label = TRUE, week_start = getOption("lubridate.week.start", 1)), # week start on monday
+    Month = month(Date_Time, label = TRUE), # ordered factor
+    Month_Day = mday(Date_Time), # numeric
+    Year = as.ordered(year(Date_Time)), # ordered factor
+    Year_Day = mday(Date_Time), # numeric
+    Hour = hour(Date_Time), # numeric
+    Minute = minute(Date_Time), # numeric
+    min_dec = as.numeric(paste(Hour, Minute*100/60, sep = ".")) # numeric
+  ) 
+
+# subset
+df1 <- df[c(117313:117984),] 
+df1$Type <- "Correct"
+
+# add missing values
+df1$Total_Power[c(120:140)] <- NA
+df1$Total_Power[c(500:560)] <- NA
+
+df1$Type[c(120:140, 500:560)] <- "Missing Value"
+
+# add outliers and inconsistances
+df1$Total_Power[234] <- 1933
+df1$Total_Power[449] <- 1200
+df1$Type[c(234,449)] <- "Outlier"
+
+df1$Total_Power[321] <- -100
+df1$Total_Power[212] <- -100
+df1$Total_Power[243] <- -110
+df1$Type[c(321, 212, 243)] <- "Inconsistence"
+
+write.csv(df1,"./data/week_not_clean.csv", row.names = FALSE)
+
+df1 %>%
+ggplot() + 
+geom_line(aes(x = as.POSIXct(Date_Time, format = "%H:%M:%S" , tz = "Europe/Rome") , y = Total_Power) , 
+          color = "grey", size = 0.7) +
+  scale_x_datetime(
+    breaks = date_breaks("12 hour"),                     # specify breaks every 4 hours
+    labels = date_format(("%H:%M") , tz="Etc/GMT+12"),  # specify format of labels
+    expand = c(0,0)                                     # expands x axis
+  ) +
+  scale_y_continuous(
+    limits = c(0,ceiling(max(df1$Total_Power)) +10 ),       # set limits from 0 to higher power consumption
+    expand=c(0,0)                                       # expands x axis
+  ) +
+  theme_bw() +
+  ggplot2::theme(
+                 axis.title.x = element_text(size = 12, margin = margin(t = 20, r = 20, b = 0, l = 0)),
+                 axis.title.y = element_text(size = 12, margin = margin(t = 20, r = 20, b = 0, l = 0)),
+                 axis.text.x = element_text(size = 11, angle = 45, vjust = .0),
+                 axis.text.y = element_text(size = 11 , vjust = .4),
+                 panel.background = element_rect(fill = "white")
+  ) +
+  labs(x = "Hour" , y = "Power [kW]")
+
